71a9135f05d5ef0ed955db1fee1a1937fd01bd5f
toseaf.s@somaiya.edu <Tausif Shaikh>
2025-07-22 13:25:40 +0530

    add tsconfig

--- src/controllers/message.controllers.ts
+++ src/controllers/message.controllers.ts
@@ -5,13 +5,9 @@ import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiResponse } from "../utils/ApiResponse";
 import { emitSocketEvent } from "../socket";
-import {
-  getLocalPath,
-  getStaticFilePath,
-  removeLocalFile,
-} from "../utils/helpers";
-import { uploadOnCloudinary } from "../utils/cloudinary";
-import { ChatEventEnum } from "../../constants";
+import { deleteFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary";
+import { ChatEventEnum } from "../constants";
+import { Request, Response } from "express";
 
 // Utility function which returns the pipeline stages to structure the chat message schema with common lookups
 // returns {mongoose.PipelineStage[]}
@@ -43,7 +39,7 @@ const chatMessageCommonAggregation = () => {
   ];
 };
 
-const getAllMessages = asyncHandler(async (req, res) => {
+const getAllMessages = asyncHandler(async (req: Request, res: Response) => {
   const { chatId } = req.params;
 
   const selectedChat = await Chat.findById(chatId);
@@ -87,68 +83,143 @@ const sendMessage = asyncHandler(async (req, res) => {
 
   if (!selectedChat) throw new ApiError(404, "Chat doesn't exist");
 
-
-  let messageFiles
+  let messageFiles;
   if (req.files && req.files?.attachments.length > 0) {
-
     messageFiles = await Promise.all(
-  req.files?.attachments?.map(async (attachment) => {
-    const url = await uploadOnCloudinary(attachment.path);
-    return { url };
-  }) || []  // fallback in case `attachments` is undefined
-);
-
+      req.files?.attachments?.map(async (attachment) => {
+        const res = await uploadOnCloudinary(attachment.path);
+        const url = res?.secure_url;
+        return { url };
+      }) || [] // fallback in case `attachments` is undefined
+    );
   }
 
-//   Create a new Message instance with appropriat metadata
+  //   Create a new Message instance with appropriat metadata
   const message = await ChatMessage.create({
-    sender : new mongoose.Types.ObjectId(req.user._id),
-    content : content || "",
-    chat : new mongoose.Types.ObjectId(chatId),
-    attachments : messageFiles
-  })
+    sender: new mongoose.Types.ObjectId(req.user._id),
+    content: content || "",
+    chat: new mongoose.Types.ObjectId(chatId),
+    attachments: messageFiles,
+  });
 
-    // update the chat's last message which could be utilized to show last message in the list item
-    const chat = await Chat.findByIdAndUpdate(
-        chatId,
-        {
-            $set : {
-                lastMessage : message._id
-            }
-        },
-        { new : true }
-    )
+  // update the chat's last message which could be utilized to show last message in the list item
+  const chat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $set: {
+        lastMessage: message._id,
+      },
+    },
+    { new: true }
+  );
 
-    // Structure the message and fill all the sender user's document
-    const messages = await ChatMessage.aggregate([
-        {
-            $match : {
-                _id : new mongoose.Types.ObjectId(chatId)
-            }
-        },
-        ...chatMessageCommonAggregation()
-    ])
+  // Structure the message and fill all the sender user's document
+  const messages = await ChatMessage.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(chatId),
+      },
+    },
+    ...chatMessageCommonAggregation(),
+  ]);
 
-    // Store all the messages and the aggregation result
-    const receivedMessage = messages[0]
+  // Store all the messages and the aggregation result
+  const receivedMessage = messages[0];
 
-    if(!receivedMessage) throw new ApiError(500, "Internal server error")
+  if (!receivedMessage) throw new ApiError(500, "Internal server error");
 
-    // logic to emit socket event about the new message created to the other participants
-    chat?.participants?.forEach((participantObjectId) => {
-        // here the chat is the raw instance of the chat in which participants is the array of object ids of users
+  // logic to emit socket event about the new message created to the other participants
+  chat?.participants?.forEach((participantObjectId) => {
+    // here the chat is the raw instance of the chat in which participants is the array of object ids of users
     // avoid emitting event to the user who is sending the message
-    if(participantObjectId.toString() === req.user._id.toString()) return 
+    if (participantObjectId.toString() === req.user._id.toString()) return;
 
     // emit the receive message event to the other participants with received message as the payload
     emitSocketEvent(
+      req,
+      participantObjectId,
+      ChatEventEnum.MESSAGE_RECEIVED_EVENT,
+      receivedMessage
+    );
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, receivedMessage, "Message Saved successfully"));
+});
+
+const deleteMessage = asyncHandler(async (req, res) => {
+  const { chatId, messageId } = req.params;
+
+  const chat = await Chat.findOne({
+    _id: new mongoose.Types.ObjectId(chatId),
+    participants: req.user._id,
+  });
+
+  if (!chat) throw new ApiError(404, "Chat doesn't exist");
+
+  // Find the message based on messageId
+  const message = await ChatMessage.findOne({
+    _id: new mongoose.Types.ObjectId(),
+    chat: new mongoose.Types.ObjectId(chatId),
+  });
+
+  if (!message) throw new ApiError(404, "Message doesn't exist");
+
+  // Check if user is the sender of this message or not
+  if (!message.sender?.toString() !== req.user?._id.toString()) {
+    throw new ApiError(
+      403,
+      "You are not authorized to delete this message, You are not the sender"
+    );
+  }
+
+  if (Array.isArray(message.attachments) && message.attachments.length > 0) {
+    // If the message is an Attachment remove the message from the cloudinary
+    await Promise.all(
+      message.attachments.map(async (asset) => {
+        await deleteFromCloudinary(asset.url);
+      })
+    );
+  }
+
+  // Deleting the message from cloudinary
+  await ChatMessage.deleteOne({
+    _id: new mongoose.Types.ObjectId(messageId),
+  });
+
+  //Updating the last message of the chat to the previous message after deletion if the message deleted was last message
+  if (chat.lastMessage?.toString() === message._id!.toString()) {
+    const lastMessage = await ChatMessage.findOne(
+      { chat: chatId },
+      // Return all the fields of the filtered document
+      {},
+      { sort: { createdAt: -1 } }
+    );
+
+    await Chat.findByIdAndUpdate(chatId, {
+      lastMessage: lastMessage ? lastMessage._id : null,
+    });
+  }
+
+  // logic to emit socket event about the message deleted  to the other participants
+  chat.participants?.forEach((participantObjectId) => {
+    // here the chat is the raw instance of the chat in which participants is the array of object ids of users
+    // avoid emitting event to the user who is deleting the message
+    if(participantObjectId.toString() ===  req.user._id.toString()) return;
+
+    // Emit the delete message event to other participants to front-end with delete messageId as payload
+    emitSocketEvent(
         req,
         participantObjectId,
-        ChatEventEnum.MESSAGE_RECEIVED_EVENT,
-        receivedMessage
+        ChatEventEnum.MESSAGE_DELETE_EVENT,
+        message
     )
-    })
 
-    return res.status(200).json(new ApiResponse(200,receivedMessage, "Message Saved successfully"))
+  });
 
+  return res.status(200).json(new ApiResponse(200, message, "Message deleted successfully"))
 });
+
+
+export {getAllMessages, sendMessage, deleteMessage}
--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares";
-import { getAllChats } from "../controllers/chat.controllers";
+import { addNewParticipantInGroupChat, createAGroupChat, createOrGetAOneOnOneChat, deleteGroupChat, deleteOneOnOneChat, getAllChats, getGroupChatDetails, leaveGroupChat, removeParticipantFromGroupChat, renameGroupChat, searchAvailableUsers } from "../controllers/chat.controllers";
+import { mongoIdPathVariableValidator } from "../validators/mongodb.validator";
+import { validate } from "../validators/validate";
+import { createAGroupChatValidator, updateGroupChatNameValidator } from "../validators/chat.validator";
 
 
 const router = Router()
@@ -10,6 +13,59 @@ router.use(verifyJWT);
 
 router.route("/").get(getAllChats)
 
+router.route("/users").get(searchAvailableUsers)
 
+router
+    .route("/c/:receiverId")
+    .post(
+        mongoIdPathVariableValidator,
+        validate,
+        createOrGetAOneOnOneChat
+    )
+
+router.route("/group")
+        .post(
+            createAGroupChatValidator(), validate, createAGroupChat
+        )
+
+router.route("/group/:chatId")
+    .get(mongoIdPathVariableValidator("chatId"), validate, getGroupChatDetails)
+    .patch(
+        mongoIdPathVariableValidator("chatId"),
+        updateGroupChatNameValidator(),
+        validate,
+        renameGroupChat
+    )
+    .delete(
+        mongoIdPathVariableValidator("chatId"),
+        validate,
+        deleteGroupChat
+    )
+
+
+router.
+    route("/group/:chatId/:participantId")
+    .post(
+        mongoIdPathVariableValidator("chatId"),
+        mongoIdPathVariableValidator("participantId"),
+        validate,
+        addNewParticipantInGroupChat
+    )
+    .delete(
+        mongoIdPathVariableValidator("chatId"),
+        mongoIdPathVariableValidator("participantId"),
+        validate,
+        removeParticipantFromGroupChat
+    )
+
+router.
+    route("/leave/group/:chatId")
+    .delete(
+        mongoIdPathVariableValidator("chatId"),
+        validate,
+        leaveGroupChat
+    )
+
+router.route("/remove/:chatId").delete(mongoIdPathVariableValidator("chatId"),validate, deleteOneOnOneChat)
 
 export default router;
